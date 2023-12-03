2c7e483917fee2096446422e52a1e46ea80eba16
Gurnav224 <chaudharyg856@gmail.com>
2023-12-03 21:20:15 +0530

    hamburger menu done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log("hello world")
+const hamburgerMenu = document.querySelector(".hamburger__menu");
+const navListContainer = document.querySelector(".navList__container");
+const hamburgerClose = document.querySelector(".hamburger__close")
+
+
+console.log(hamburgerMenu)
+console.log(navListContainer)
+
+hamburgerMenu.addEventListener("click",()=>{
+
+    if(navListContainer.style.display==="none"){
+
+        navListContainer.style.display = "block"
+    }
+    else{
+        navListContainer.style.display = "none"
+    }
+
+
+    
+})
+
+hamburgerClose.addEventListener('click',()=>{
+    if(navListContainer.style.display==="block"){
+
+        navListContainer.style.display = "none"
+    }
+    else{
+        navListContainer.style.display = "block"
+    }
+
+})
